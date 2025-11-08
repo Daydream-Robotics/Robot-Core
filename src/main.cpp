a69a7fc1b27f3e3542ca8f1b24b058baa82c505a
Natalia Cano <140561361+ncano1025@users.noreply.github.com>
2025-11-08 11:36:51 -0500

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,8 @@ void opcontrol() {
 	// Set chassis brake mode to coast
 	leftMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_COAST);
 	rightMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_COAST);
-	
-	while(true){
 
+	while(true){
 		// Get joystick values
 		int leftY = controller.get_analog(ANALOG_LEFT_Y);
 		int rightY = controller.get_analog(ANALOG_RIGHT_Y);
