e02fe3928787771ae69a8f427f286737bf1bb8e5
BatsCrow <twilliamsa776@gmail.com>
2025-11-13 21:15:35 -0500

    Implementation using get_yaw_quat

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -12,32 +12,52 @@ double pos_x = 0;
 double pos_y = 0;
 double theta = 0;
 
+double get_yaw_quaternion() {
+	pros::quaternion_s_t qt = imu.get_quaternion();
+
+	//error fetching quat, retry
+	if (qt.w == PROS_ERR_F) {
+		qt = imu.get_quaternion();
+		if (qt.w == PROS_ERR_F) {
+			// pros::lcd::set_text(5, "ERROR: IMU Quaternion Fetch Failed");
+			return -1.0;
+		}
+	}
+
+	//convert quat to yaw
+	double yaw = atan2(2 * ((qt.w * qt.z) + (qt.x * qt.y)), 1 - (2 * ((qt.y * qt.y) + (qt.z * qt.z)))); //yaw formula = atan2(2(wz + xy), 1 - 2(y^2 + z^2))
+
+	//returns yaw converted from rad to deg; angle is returned from -180 to 180 (+ 180 for [0, 360])
+	return ((yaw * (180 / M_PI)) + 180);
+}
 
 void update_position_and_angle() {
 	// Calculate distance travelled by each tracking wheel
 	ArcLengths arcs = get_wheel_travel();
 
 	// Get orientation from
-	theta = convertDegToRad(imu.get_yaw());
+	theta = convertDegToRad(get_yaw_quaternion());
 
 	// Determine change in heading 
 	double del_theta = theta - prevTheta;
+
+	// Bound the change in angle between [-pi, pi]
+	del_theta = fmod(del_theta, M_PI);
+	if (del_theta < -M_PI) {
+		del_theta += M_PI;
+	}
 
 	// Determine change in local x and in local y
 	double dx_local = (arcs.left + arcs.right) / 2.0;
 	double dy_local = arcs.back - (del_theta * BACK_TRACKING_WHEEL_DISTANCE);
 
-	// Compute using midpoint formula
-	double heading_mid = theta + (del_theta / 2);
-
     // Compute change in x and y based on heading and local changes
-	double del_x = std::cos(heading_mid) * dx_local - std::sin(heading_mid) * dy_local;
-	double del_y = std::sin(heading_mid) * dx_local + std::cos(heading_mid) * dy_local;
+	double del_x = std::cos(theta) * dx_local - std::sin(theta) * dy_local;
+	double del_y = std::sin(theta) * dx_local + std::cos(theta) * dy_local;
 
 	// Increment position and angle by calculated changes
 	pos_x += del_x;
 	pos_y += del_y;
-	theta += del_theta;
 
 	prevTheta = theta;
 }
