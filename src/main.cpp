9b22d57a8b2a45c0587269d1b50d4762727059aa
BatsCrow <twilliamsa776@gmail.com>
2026-02-17 21:56:50 -0500

    auto matchload and backup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "subsystems.h"
 #include "constants.h"
-#include "odometry.h"
+#include "autonomous.hpp"
 
 #include <numbers>
 
@@ -21,53 +21,64 @@ void competition_initialize() {}
 
 void autonomous() {
 
-	// Delay to sync with 15" bot
-	pros::delay(30000);
+	Autonomous auton = Autonomous();
 
-	leftMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_HOLD);
-	rightMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_HOLD);
+	// // Intake pre-loads
+	move_intake(STOP, HIGH_VOLTAGE, HIGH_VOLTAGE);
+	auton.travel(8, 40, 0);
+	// travelDistanceWithHeading(8, 40, 0, -1);
+	// pros::delay(250);
 
-	// Intake pre-loads
-	move_intake(HIGH_VOLTAGE);
-	travelDistanceWithHeading(8, 40, 0, -1);
-	pros::delay(250);
+	// // Backup to match loader
+	auton.travel(-42.5, 70, 0);
+	// travelDistanceWithHeading(-35.25, 70, 0, -1);
 
-	// Backup to match loader
-	travelDistanceWithHeading(-35.25, 70, 0, -1);
+	// // Face match loader
+	move_intake(STOP, STOP, STOP);
+	auton.turnTo(90);
+	// turn_pid(90, 0);
 
-	// Face match loader
-	move_intake(STOP);
-	turn_pid(90, 0);
-
-	// Match load
+	// // Match load
 	unloader.set_value(true);
-	travelDistanceWithHeading(14, 70, 90, 1350);
-	move_intake(HIGH_VOLTAGE, STOP);
-	travelDistanceWithHeading(100, 35, 90, 1000);
-	pros::delay(100);
-	travelDistanceWithHeading(100, 25, 90, 1000);
-	pros::delay(200);
-	travelDistanceWithHeading(100, 35, 90, 1000);
+	pros::delay(250);
+	move_intake(STOP, HIGH_VOLTAGE, HIGH_VOLTAGE);
+	auton.travel(16, 90, 90, 1.150);
+	// travelDistanceWithHeading(14, 70, 90, 1350);
+	// travelDistanceWithHeading(100, 35, 90, 1000);
+	auton.travel(-2, 50, 90, 0.500);
+	pros::delay(3000);
+	move_intake(STOP);
+	// travelDistanceWithHeading(100, 25, 90, 1000);
+
+	// travelDistanceWithHeading(100, 35, 90, 1000);
 
 
-	// Reverse to Long Goal
-	travelDistanceWithHeading(-50, 60, 90, 2000);
+	// // Reverse to Long Goal
+	// travelDistanceWithHeading(-50, 60, 90, 2000);
+	auton.travel(-50, 80, 92, 2);
+	move_intake(-MID_VOLTAGE, -MID_VOLTAGE, STOP, 0.2);
+	move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE, HIGH_VOLTAGE, 3);
 
-	move_intake(-MID_VOLTAGE, -MID_VOLTAGE, 0.2);
-	// Score on Long Goal
-	move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE, 3);
-	unloader.set_value(false);
+	auton.travel(20, 80, 90);
+
+	auton.turnTo(0);
+	auton.travel(70, 100, 45, 3);
+
+	// move_intake(-MID_VOLTAGE, -MID_VOLTAGE, 0.2);
+	// // Score on Long Goal
+	// move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE, 3);
+	// unloader.set_value(false);
 
 
-	// Backup and face side balls
-	travelDistanceWithHeading(16, 70, 90, -1);
-	turn_pid(0, 0);
+	// // Backup and face side balls
+	// travelDistanceWithHeading(16, 70, 90, -1);
+	// turn_pid(0, 0);
 	
-	move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE);
-	// Park
-	travelDistanceWithHeading(40, 100, 115, 1900);
+	// move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE);
+	// // Park
+	// travelDistanceWithHeading(40, 100, 115, 1900);
 	
-	move_intake(STOP, STOP);
+	// move_intake(STOP, STOP);
 
 }
 
@@ -80,8 +91,6 @@ void opcontrol() {
 	backIntake.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
 
 	while(true){
-
-		update_position_and_angle();
 	
 		/* - - - - - - - - - - - - - - [CHASSIS CONTROLS] - - - - - - - - - - - - - - */
 
@@ -99,9 +108,6 @@ void opcontrol() {
 			centerScore.toggle();
 		}
 
-		if (controller.get_digital(DIGITAL_B)) {
-			move_intake(-HIGH_VOLTAGE, -HIGH_VOLTAGE);
-		}
 
 		/* - - - - - - - - - - - - - - [DESCORE TOGGLE] - - - - - - - - - - - - - - */
 
@@ -113,13 +119,13 @@ void opcontrol() {
 
 		// Main intake
 		if (controller.get_digital(DIGITAL_R1)) { // intake
-			move_intake(HIGH_VOLTAGE);
+			move_intake(STOP, HIGH_VOLTAGE, HIGH_VOLTAGE);
 		} else if (controller.get_digital(DIGITAL_R2)){ // outtake
-			move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE);
+			move_intake(HIGH_VOLTAGE, HIGH_VOLTAGE, HIGH_VOLTAGE);
 		} else if (controller.get_digital(DIGITAL_L1)){ // score
 			//descorer.toggle();
 		} else if (controller.get_digital(DIGITAL_B)) { // full outtake
-			move_intake(-HIGH_VOLTAGE, -HIGH_VOLTAGE);
+			move_intake(-HIGH_VOLTAGE, -HIGH_VOLTAGE, -HIGH_VOLTAGE);
 		} else {
 			move_intake(STOP);
 		}
@@ -129,12 +135,12 @@ void opcontrol() {
 	}
 }
 
-void move_intake(int front, int back, double seconds) {
+void move_intake(int front, int mid, int back, double seconds) {
 
 	// check for stalling later and stop motors if stalling
 
 	frontIntake.move(front);
-	midIntake.move(front);
+	midIntake.move(mid);
 	backIntake.move(back);
 
 	if (seconds != 0) {
