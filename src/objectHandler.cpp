6ed2c383224a9a5d878260ada163cb9d0cbe198c
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2026-04-16 21:25:13 -0400

    added importance notice of A prints

--- src/objectHandler.cpp
+++ src/objectHandler.cpp
@@ -50,7 +50,7 @@ void UpdateFrame_task_fn(void* param){
         // pros::lcd::print(7, "%d", counter++);
         
         //begin serial handshake
-        printf("A\n");
+        printf("A\n");  // !IMPORTANT! Do NOT remove, Keeps AI alive
         fflush(stdout);
 
         // pros::lcd::set_text(3, "waiting for input");       
