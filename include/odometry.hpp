6a2cf596cb812a081708524e8a4d0b54a095809c
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2026-05-07 18:07:58 -0400

    Refactored odometry class with documentation, task safety, and small previous state adjustments

--- include/odometry.hpp
+++ include/odometry.hpp
@@ -1,22 +1,71 @@
 #pragma once
+#include "pros/rtos.hpp"
 
-typedef struct WheelLengths {
+struct WheelLengths {
     double parallel;
     double perpendicular;
-} WheelLengths;
+};
 
-typedef struct Position {
+struct Position {
     double x;
     double y;
-} Position;
+};
 
+/**
+ * @brief stores a pose of the robot
+ * @note Made for the frame: +X forward, +Y left, CCW positive
+ */
+struct Pose {
+    // x-position of bot (inches)
+    double x;
+
+    // y-position of bot (inches)
+    double y;
+
+    // Heading of bot (rads)
+    double theta;
+};
+
+/**
+ * @class Odometry
+ * @brief Tracks robots global position and heading on the field
+ */
 class Odometry {
-
+    
 public:
 
-
-    // Update position and orientation
+    /**
+     * @brief Calculates and updates the robot's global pose
+     * @note This function is safe to be called continuously in a background task
+     * @warning In the case of an IMU failure this function stops updating the latest position
+     */
     void updatePose(void);
+
+    /**
+     * @brief Gets the robot's latest pose
+     * @returns The latest Pose of the robot
+     */
+    Pose getPose();
+
+    /**
+     * @brief Sets the robot's current position
+     * @param pose The starting pose of the robot
+     */
+    void setPose(Pose pose);
+
+    /**
+     * @brief Gets the robots x position
+     * @note task safe
+     * @returns The global X coordinate of the robot in inches
+     */
+    double getPosX();
+
+    /**
+     * @brief Gets the robots y position
+     * @note task safe
+     * @returns The global Y coordinate of the robot in inches
+     */
+    double getPosY();
 
     /** 
      * @brief gets the yaw of the robot
@@ -25,31 +74,37 @@ public:
      * @retval	-180.1	IMU disconnected
      * @retval	-180.2	IMU calibrating
      * @retval	-180.3	Pros communication failure
-    */
+     */
     double getYaw(void);
     
-    double getPosX();
-    double getPosY();
-
-    // Set the robot's current position
-    void setPose(double x, double y);
+    /**
+     * @brief Returns struct of distances travelled by Odometry Wheels
+     */
+    WheelLengths getOdomWheelTravel(void);
     
-    // Background task entry point
+    /**
+     * @brief gets the current velocity of the parallel tracking wheel
+     * @returns velocity of parallel tracking wheel in inches per second
+     */
+    double getParallelVel();
+    
+    /**
+     * @brief Background task entry point
+     */
     static void odomTask();
 
-    // Returns struct of distances travelled by Odometry Wheels
-    WheelLengths getOdomWheelTravel(void);
+private:
+    // stores the latest global position and heading on the robot
+    Pose m_currentPosition = {0, 0, 0};
 
-    double getParallelVel();
-
-    // x-position of bot (inches)
-    double pos_x = 0.0;
-
-    // y-position of bot (inches)
-    double pos_y = 0.0;
-
-    // Heading of bot (rads)
-    double heading = 0.0;
+    // ROTS mutex to keep data task safe
+    pros::Mutex m_mutex;
+  
+    // previous state tracking
+    double m_prevTheta = 0;
+    double m_prevParallel = 0;
+    double m_prevPerpendicular = 0;
+    bool m_initialized = false;
 };
 
 extern Odometry odom;
