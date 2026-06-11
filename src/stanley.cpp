f62505750de7bd9dd8f6244b780ec9e12fe732c2
Brandon Stile <bgstile06@gmail.com>
2026-06-11 14:51:32 -0400

    Print Statements for Debugging and Data use

--- src/stanley.cpp
+++ src/stanley.cpp
@@ -22,16 +22,21 @@ bool StanleyController::step() {
     double x   = odom.getPosX();
     double y   = odom.getPosY();
     double yaw = odom.getYaw(); 
+    printf("X: %d   Y: %d   Yaw: %d\n", x, y, yaw);
 
     m_closestIdx = m_als_path->findClosestSampleIndex({x, y}, m_closestIdx);
     const Sample& nearest = m_als_path->getSamples()[m_closestIdx];
 
     double distFromEnd = m_als_path->getTotalLength() - nearest.s;
+    printf("End Distance%d\n", distFromEnd);
+
     if (distFromEnd < STANLEY_END_TOL) {
         leftMotors.move_velocity(0);
         rightMotors.move_velocity(0);
+        printf("REACHED END with a %d error", distFromEnd);
         return true;
     }
+    
 
 
     double pathYawRad  = nearest.heading;
@@ -45,10 +50,13 @@ bool StanleyController::step() {
     double cte = -std::sin(pathYawRad) * ex + std::cos(pathYawRad) * ey;
 
     double speed = std::abs(odom.getParallelVel()); 
-
+    printf("Speed: %d", speed);
 
     double steer = headingError + std::atan2(STANLEY_K * cte, speed + STANLEY_K_SOFT);
+    printf("Steer Before Clamp: %d", steer);
+
     steer = std::clamp(steer, -STANLEY_MAX_STEER, STANLEY_MAX_STEER);
+    printf("Steer After Clamp: %d", steer);
 
     int baseVel = STANLEY_MAX_VEL;
     if (distFromEnd < STANDLEY_END_SLOW) {
@@ -57,19 +65,28 @@ bool StanleyController::step() {
         baseVel = std::max(baseVel, STANLEY_MIN_VEL);
     }
 
+    printf("Base Velocity: %d\n", baseVel);
+
     double leftVel  = (baseVel - steer * STANLEY_TURN_SCALE);
     double rightVel = (baseVel + steer * STANLEY_TURN_SCALE);
-
+    
+    printf("leftMotors Unscaled Velocity: %d\nrightMotors Unscaled Velocity: %d\n", leftVel, rightVel);
+    
    
     double maxMag = std::max(std::abs(leftVel), std::abs(rightVel));
     if (maxMag > STANLEY_MAX_VEL) {
         double scale = STANLEY_MAX_VEL / maxMag;
         leftVel  *= scale;
         rightVel *= scale;
+        printf("Scale: %d\nleftMotors Scaled Velocity: %d\nrightMotors Scaled Velocity: %d\n", scale, leftVel, rightVel);
     }
+
+    else printf("No Velocity Scale");
 
     leftMotors.move_velocity(static_cast<int>(leftVel));
     rightMotors.move_velocity(static_cast<int>(rightVel));
+
+    
 
     return false;
 }
