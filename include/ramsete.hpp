abf9c3489be815e0ec37e3e6dfbea858f30a913f
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2026-06-21 02:29:59 +0200

    changed to be child of motion controller

--- include/ramsete.hpp
+++ include/ramsete.hpp
@@ -1,34 +1,18 @@
 #pragma once
 #include "constants.h"
 #include "odometry.hpp"
+#include "motionController.hpp"
 
 struct RamseteConfig {
-    double b;
-    double zeta;
+    double b;                   // proportional gain (higher means robot converges to path more agressively) default: 2
+    double zeta;                // damping ratio (higher zeta smooths out oscillations but slows convergence) defaul: 0.7
     double trackWidthInches;
 };
-
-struct WheelVelocities {
-    double left;
-    double right;
-}
 
 /**
  * @class Ramsete Controller
  */
-class RamseteController {
-    public:
-        /**
-         * @brief Constructs Ramsete controller instance
-         * @param config The config for the ramsete controller
-         */
-        RamseteController(RamseteConfig config);
-
-        /**
-         * @brief Computes the commanded wheel velocities
-         */
-        WheelVelocities compute(Pose currentPose, Pose targetPose, double targetLinearVel, double targetAngularVel);
-
+class RamseteController : public MotionController {
     private:
         double m_b;
         double m_zeta;
@@ -38,4 +22,19 @@ class RamseteController {
          * @brief computes sin(x)/x with divide by zero catch
          */
         double sinc(double x);
+
+    public:
+        /**
+         * @brief Constructs Ramsete controller instance
+         * @param config The config for the ramsete controller
+         */
+        RamseteController(RamseteConfig config);
+        RamseteController(double b, double zeta, double trackWidthInches);
+        
+        virtual ~RamseteController() = default;
+
+        /**
+         * @brief Computes the commanded wheel velocities
+         */
+        WheelVelocities compute(const Pose& currentPose, const ALS_Path& als_path, std::size_t& closestSampleIdx, PathFlag flag) override;
 };
