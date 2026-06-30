538e8e5aa4593e64f578a427f4a9e19702c7b102
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2026-06-30 19:27:54 -0400

    add comments to fieldLogger and update main

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,8 @@ void autonomous() {
         velocity_log.log(leftMotors.get_actual_velocity(0), pros::millis()/1000.0);
 		pros::delay(20);
 	}
-	pros::lcd::print(0, "DOne");
+	sin_log.flush();
+	sin_log.close();
 	printf("[MAIN] FIRST_PATH tracking complete.\n");
     pros::lcd::print(5, "done");
 
@@ -81,10 +82,10 @@ void autonomous() {
 	printf("[MAIN] Delaying 2000ms...\n");
 	pros::delay(2000);
 
-	// if (paths.size() <= PathName::SECOND_PATH) {
-	// 	printf("[MAIN-ERROR] SECOND_PATH index out of bounds! Array size is %zu\n", paths.size());
-	// 	return;
-	// }
+	if (paths.size() <= PathName::SECOND_PATH) {
+		printf("[MAIN-ERROR] SECOND_PATH index out of bounds! Array size is %zu\n", paths.size());
+		return;
+	}
 
 	printf("[MAIN] Setting SECOND_PATH...\n");
 	pathFollower->setPath(paths[PathName::SECOND_PATH], PathFlag::REVERSE, true, "test_reverse");
