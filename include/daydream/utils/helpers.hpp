6c9567b8ca4dea9258cbcb094c10a692d4c589ce
l1jiayi <jennyli112904@gmail.com>
2026-09-22 20:44:38 -0400

    comments from deg -> rad

--- include/daydream/utils/helpers.hpp
+++ include/daydream/utils/helpers.hpp
@@ -2,7 +2,7 @@
 
 #include "daydream/motion/odometry.hpp" // Needed for the Position struct
 
-// Normalize angle between [-180, 180] 
+// Normalize angle between [-pi,pi] 
 double normalizeAngle(double a);
 
 // Return Euclidean distance btwn points p1 and p2
